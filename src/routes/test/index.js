8aa9975406984fe38991298b37710bba1b8cc2e7
jhlee <jhlee@spicusinc.com>
2022-08-05 19:03:18 +0900

    edit service

--- src/routes/test/index.js
+++ src/routes/test/index.js
@@ -9,29 +9,38 @@ const Test = () => {
 	const [perPage, setPerPage] = useState(20);
 	const offset = (pageNo - 1) * perPage;
 
-	console.log(questions);
-	questions.slice(offset, offset + perPage).map(element => console.log(element));
+	const handleClick = function (index, value) {
+		let _answers = answers;
+		_answers[index] = value;
+		setAnswers(_answers);
+		console.log(answers);
+	}
 
 	return (
-		<div class={style.test}>
-			{
-				questions
-					.slice(offset, offset + perPage)
-					.map(({ type, question }, index) => (
-						<article key={index}>
-							<div>
-								<p>{offset + index + 1}. {question}</p>
-							</div>
-							<div className={style.btnWrap}>
-								<button className={style.btn}>1점</button>
-								<button className={style.btn}>2점</button>
-								<button className={style.btn}>3점</button>
-								<button className={style.btn}>4점</button>
-								<button className={style.btn}>5점</button>
-							</div>								
-						</article>
-					))
-			}
+		<div className={style.test}>
+			<div className={style.articleWrap}>
+				{
+					questions
+						.slice(offset, offset + perPage)
+						.map(({ type, question }, index) => (
+							<article key={index}>
+								<div>
+									<p>{offset + index + 1}. {question}</p>
+								</div>
+								<div className={style.btnWrap}>
+									<button className={style.btn} onClick={handleClick}>1점</button>
+									<button className={style.btn} onClick={handleClick}>2점</button>
+									<button className={style.btn} onClick={handleClick}>3점</button>
+									<button className={style.btn} onClick={handleClick}>4점</button>
+									<button className={style.btn} onClick={handleClick}>5점</button>
+								</div>								
+							</article>
+						))
+				}
+			</div>
+			<div className={style.buttonWrap}>
+				<button>Next</button>
+			</div>
 		</div>
 	)
 };
